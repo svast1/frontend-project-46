ff67b9113ff88078a370e259467bf11f862c998b
svast1 <dubboylbox@mail.ru>
2023-03-22 13:08:03 +0300

    step 6

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -12,9 +12,9 @@ program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format')
-  .action((filepath1, filepath2) => {
-    const diff = genDiff(filepath1, filepath2);
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .action((filepath1, filepath2, options) => {
+    const diff = genDiff(filepath1, filepath2, options.format);
     console.log(diff);
   });
 
