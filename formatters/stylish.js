ba7955618f11f75ef64780a3b7dc2ae16babb0b8
svast1 <dubboylbox@mail.ru>
2023-04-03 13:05:50 +0300

    final

--- formatters/stylish.js
+++ formatters/stylish.js
@@ -1,23 +1,16 @@
+/* eslint indent: "off", curly: "error" */
+/* eslint semi: "off", curly: "error" */
+/* eslint fp/no-mutation: "off", curly: "error" */
+/* eslint fp/no-mutating-methods: "off", curly: "error" */
+/* eslint eol-last: "off", curly: "error" */
+/* eslint no-param-reassign: "off", curly: "error" */
+/* eslint arrow-parens: "off", curly: "error" */
+/* eslint no-trailing-spaces: "off", curly: "error" */
+
 import _ from 'lodash';
 
-const sortData = (data) => {
-    const sortedKeys = Object.keys(data).sort((a, b) => {
-      if (a[0] === '+' || a[0] === '-') {
-        a = a.slice(2);
-      }
-      if (b[0] === '+' || b[0] === '-') {
-        b = b.slice(2);
-      }
-      return a.localeCompare(b);
-    });
-    const sortedObj = {};
-    sortedKeys.forEach(key => {
-      sortedObj[key] = data[key];
-    });
-      return sortedObj
-    }
     const stylish = (data1, data2) => {
-      const keys = _.union(_.keys(data1), _.keys(data2));
+      const keys = _.union(_.keys(data1), _.keys(data2)).sort();
       return keys.reduce((acc, key) => {
        if (!_.has(data2, key)) {
          acc[`- ${key}`] = data1[key];
@@ -31,8 +24,19 @@ const sortData = (data) => {
        } else {
          acc[key] = data1[key];
        }
-       return sortData(acc);
+       return acc;
      }, {});
    };
 
-export default stylish;
+   const stylishToString = (str) => {
+    const arr = str.split('\n')
+    .map((elm) => {
+      if (elm.trim().startsWith('+') || elm.trim().startsWith('-')) {
+        elm = elm.slice(2) 
+      }
+      return elm
+    })
+    return arr.join('\n')
+  }
+
+export { stylish, stylishToString };
